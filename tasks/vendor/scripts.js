79487d055892f86c97ca86f95a32454f863444e0
M-SIGMA <m.phillipshider@gmail.com>
2015-07-19 02:10:07 +0100

    fix(build): Add missing 'vendor' tasks

---
+++ tasks/vendor/scripts.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var JS_DIRECTORY = "/js/";
+
+var config = require('../../configs/build.conf.js');
+
+var args = require('yargs').argv;
+var gulp = require('gulp');
+
+var concat = require('gulp-concat');
+var connect = require('gulp-connect');
+var gulpIf = require('gulp-if');
+var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
+
+gulp.task('vendor-scripts', function () {
+  var src = config.vendor.scripts;
+  var dest = config.distDir + JS_DIRECTORY;
+  var concatFile = 'vendor.js';
+  var productionEnv = args.production; // ENV VARIABLE
+
+  return gulp.src(src)
+    .pipe(plumber())
+    .pipe(concat(concatFile))
+    .pipe(gulpIf(productionEnv, uglify()))
+    .pipe(gulp.dest(dest))
+    .pipe(connect.reload());
+});
